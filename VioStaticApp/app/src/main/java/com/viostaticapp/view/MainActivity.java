7fd9e19193374d833c15d304e35ed672e4756277
Pham Duy Dat <55871927+SeiSilver@users.noreply.github.com>
2021-06-02 21:07:19 +0700

    update gradle
    thêm mô hình MVP vào code,
    thêm lombok vào, themes, setting quyền cho app

--- VioStaticApp/app/src/main/java/com/viostaticapp/view/MainActivity.java
+++ VioStaticApp/app/src/main/java/com/viostaticapp/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.viostaticapp;
+package com.viostaticapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.viostaticapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
