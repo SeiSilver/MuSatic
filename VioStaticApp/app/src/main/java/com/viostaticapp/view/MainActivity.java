8cb131a980b2a0ecdd2f7b67d6602c368e03b9cd
Pham Duy Dat <55871927+SeiSilver@users.noreply.github.com>
2021-06-06 17:57:07 +0700

    Them phan trang
    thêm phân trang vào

--- VioStaticApp/app/src/main/java/com/viostaticapp/view/MainActivity.java
+++ VioStaticApp/app/src/main/java/com/viostaticapp/view/MainActivity.java
@@ -1,16 +1,42 @@
 package com.viostaticapp.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.viostaticapp.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+
+        navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph().getId()).build();
+
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
     }
+
 }
