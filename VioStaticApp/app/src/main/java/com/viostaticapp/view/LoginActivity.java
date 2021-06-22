6860c2ad4dfd4a1365ec9a4e40c1c408a11bb93c
VietKhoa2410 <vietkhoa2410@gmail.com>
2021-06-22 16:38:55 +0700

    Signup vs Login

--- VioStaticApp/app/src/main/java/com/viostaticapp/view/LoginActivity.java
+++ VioStaticApp/app/src/main/java/com/viostaticapp/view/LoginActivity.java
@@ -2,20 +2,108 @@ package com.viostaticapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.viostaticapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private Context context;
+
+    ProgressDialog progressDialog;
+
+    FirebaseFirestore db;
+    FirebaseAuth firebaseAuth;
+
+    EditText edtEmail, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        context = this;
+
+        progressDialog = new ProgressDialog(context);
+
+        db = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        edtEmail = findViewById(R.id.edt_login_email);
+        edtPassword=findViewById(R.id.edt_login_password);
     }
 
+    public void login(View view){
+        progressDialog.setTitle("Please wait...");
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        String sEmail = edtEmail.getText().toString().trim();
+        String sPassword = edtPassword.getText().toString().trim();
+
+        if(sEmail.isEmpty())
+            createAlert("Error", "Please enter your email!", "OK");
+        else if(!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches())
+            createAlert("Error", "Please enter a valid email!", "OK");
+        else if(sPassword.isEmpty())
+            createAlert("Error", "Please enter your password!", "OK");
+        else{
+            firebaseAuth.signInWithEmailAndPassword(sEmail, sPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    progressDialog.dismiss();
+                    FirebaseUser user = authResult.getUser();
+                    startMainActivity(user);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    if (e instanceof FirebaseAuthInvalidUserException){
+                        createAlert("Error", "This email is not registered with us!", "OK");
+                    }else if(e instanceof FirebaseAuthInvalidCredentialsException){
+                        createAlert("Error", "Invalid Password! Please try again.", "OK");
+                    }else{
+                        Toast.makeText(context, "Unable to login! Please try after some time.", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+    private void startMainActivity(FirebaseUser user){
+        if (user != null){
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    private void createAlert(String alertTitle, String alertMessage, String positiveText){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle)
+                .setMessage(alertMessage)
+                .setPositiveButton(positiveText, null)
+                .create().show();
+    }
 
     public void redirectToSignup(View view){
         Intent intent = new Intent(this, SignupActivity.class);
--- VioStaticApp/app/src/main/java/com/viostaticapp/view/SignupActivity.java
+++ VioStaticApp/app/src/main/java/com/viostaticapp/view/SignupActivity.java
@@ -3,11 +3,13 @@ package com.viostaticapp.view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,13 +17,19 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.viostaticapp.R;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -32,6 +40,7 @@ public class SignupActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
 
     FirebaseFirestore db;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,52 +56,76 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(context);
 
         db = FirebaseFirestore.getInstance();
-
+        firebaseAuth = FirebaseAuth.getInstance();
 
     }
 
     public void signup(View view){
+        progressDialog.setTitle("Registering...");
+        progressDialog.setCancelable(false);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
         String sEmail=edtEmail.getText().toString().trim();
         String sPassword = edtPassword.getText().toString().trim();
         String sRePassword = edtRePassword.getText().toString().trim();
 
-        Log.i("LogMess",sPassword+" "+sRePassword+" "+sPassword.compareTo(sRePassword));
-
-
-        if(sPassword.compareTo(sRePassword)!=0) {
-            Toast.makeText(context, "Password not match", Toast.LENGTH_SHORT).show();
-            Log.i("LogMess","Not match");
-        }
+        if(sEmail.isEmpty())
+            createAlert("Error","Please enter your email!","OK");
+        else if(!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches())
+            createAlert("Error","Please enter a valid email!","OK");
+        else if(sPassword.isEmpty())
+            createAlert("Error", "Please enter password!", "OK");
+        else if(sPassword.length() < 6)
+            createAlert("Error", "Minimum password length must be 6!", "OK");
+        else if(sPassword.compareTo(sRePassword)!=0)
+           createAlert("Error","Password not match!","OK");
         else
             uploadData(sEmail,sPassword);
+
+        progressDialog.dismiss();
+
     }
 
     private void uploadData(String sEmail, String sPassword) {
-        progressDialog.setTitle("Registering account...");
-        progressDialog.show();
-
-        String id = UUID.randomUUID().toString();
-
-        Map<String, Object> account = new HashMap<>();
-        account.put("id", id);
-        account.put("email",sEmail);
-        account.put("password",sPassword);
-
-        db.collection("Accounts").document(id).set(account)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+        firebaseAuth.createUserWithEmailAndPassword(sEmail, sPassword).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                progressDialog.setMessage("Saving...");
+                Map<String, Object> dataMap = new HashMap<>();
+                db.collection("Accounts").add(dataMap).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        progressDialog.dismiss();
-                        Toast.makeText(context,"Register successfully!",Toast.LENGTH_SHORT).show();
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(context, "User registered successfully!", Toast.LENGTH_SHORT).show();
+                        finish();
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
+                }).addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        progressDialog.dismiss();
-                        Toast.makeText(context, e.getMessage(),Toast.LENGTH_SHORT).show();
+                        createAlert("Error", "Registered successfully but couldn't save details!", "OK");
                     }
                 });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthUserCollisionException){
+                    createAlert("Error", "This email address is already registered with us!", "OK");
+                }else{
+                    Toast.makeText(context, "Failed to register! Please try again later.", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    private void createAlert(String alertTitle, String alertMessage, String positiveText){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(alertTitle)
+                .setMessage(alertMessage)
+                .setPositiveButton(positiveText, null)
+                .create().show();
     }
 
     public void redirectLogin(View view){
